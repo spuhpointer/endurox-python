d2c77bc7af35ad7e8e2d75f86a31770f18fe76b0
Lauris Vitolins <contact@mavimax.com>
2022-01-21 16:08:06 +0200

    Added missing files

---
+++ cmake_example/__init__.py
@@ -0,0 +1,17 @@
+"""Root module of your package"""
+
+from .math import mul
+
+import sys
+import os
+import time
+import ctypes
+
+flags = sys.getdlopenflags()
+
+# Need as Enduro/X XA drivers are dynamically loaded, and
+# they need to see Enduro/X runtime.
+sys.setdlopenflags(flags | ctypes.RTLD_GLOBAL)
+from ._endurox import *
+# Restore original flags
+sys.setdlopenflags(flags)
---
+++ cmake_example/math.py
@@ -0,0 +1,16 @@
+def mul(a, b):
+    """Pure Python-only function"""
+    return a * b
+
+
+#def add(a, b):
+#    """Fallback function"""    
+#    return a + b    
+
+#try:
+#    from ._math import add
+#except ImportError:
+#    pass
+
+#from cmake_example._math import add
+
