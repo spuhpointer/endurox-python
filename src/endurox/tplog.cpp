a4d78c44ef4f7c0a5bd6f4968068ef08c13ac273
Lauris Vitolins <contact@mavimax.com>
2022-05-08 12:31:11 +0300

    Works on module

---
+++ src/endurox/tplog.cpp
@@ -0,0 +1,70 @@
+/**
+ * @brief Logging API
+ *
+ * @file tplog.cpp
+ */
+/* -----------------------------------------------------------------------------
+ * Enduro/X Middleware Platform for Distributed Transaction Processing
+ * Copyright (C) 2009-2016, ATR Baltic, Ltd. All Rights Reserved.
+ * Copyright (C) 2017-2022, Mavimax, Ltd. All Rights Reserved.
+ * This software is released under MIT license.
+ * 
+ * -----------------------------------------------------------------------------
+ * MIT License
+ * Copyright (C) 2019 Aivars Kalvans <aivars.kalvans@gmail.com> 
+ * Copyright (C) 2022 Mavimax SIA
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * -----------------------------------------------------------------------------
+ */
+#include <dlfcn.h>
+
+#include <atmi.h>
+#include <tpadm.h>
+#include <userlog.h>
+#include <xa.h>
+#include <ubf.h>
+#undef _
+
+#include "exceptions.h"
+#include "ndrx_pymod.h"
+
+#include <pybind11/functional.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <functional>
+#include <map>
+
+namespace py = pybind11;
+
+/**
+ * @brief Register XATMI logging api
+ * 
+ * @param m Pybind11 module handle
+ */
+expublic void ndrxpy_register_tplog(py::module &m)
+{
+
+    //TODO
+}
+
+
+/* vim: set ts=4 sw=4 et smartindent: */
+
