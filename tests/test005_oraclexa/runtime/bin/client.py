8e17da4f70b1f4613249bf1b95783124430ac37a
Madars Vitolins <madars.vitolins@gmail.com>
2022-05-16 23:41:07 +0300

    Added timed testing

--- tests/test005_oraclexa/runtime/bin/client.py
+++ tests/test005_oraclexa/runtime/bin/client.py
@@ -5,7 +5,7 @@ import endurox as e
 import time
 import cx_Oracle
 import unittest
-
+import exutils as u
 
 class TestOracleXA(unittest.TestCase):
 
@@ -13,78 +13,80 @@ class TestOracleXA(unittest.TestCase):
 
         e.tpinit()
         e.tpopen()
+        w = u.NdrxStopwatch()
+        while w.get_delta_sec() < u.test_duratation():
 
-        e.tplog_info("clean up the DB...")
-        db = cx_Oracle.connect(handle=e.xaoSvcCtx())
-        e.tpbegin(99, 0)
-        with db.cursor() as cursor:
-             cursor.execute("delete from pyaccounts")
-        e.tpcommit(0)
-       
-        # check 0...
-        e.tpbegin(99, 0)
-        with db.cursor() as cursor:
-            cursor.execute("SELECT count(*) FROM pyaccounts")
-            self.assertEqual(cursor.fetchone()[0], 0)
-        e.tpcommit(0)
-  
-
-        e.tpbegin(99, 0)
-        # do some insert
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO1", "T_LONG_FLD":100}})
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO2", "T_LONG_FLD":102}})
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO3", "T_LONG_FLD":103}})
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO4", "T_LONG_FLD":104}})
-
-        # check account, shall be 4...
-        with db.cursor() as cursor:
-            cursor.execute("SELECT count(*) FROM pyaccounts")
-            self.assertEqual(cursor.fetchone()[0], 4)
-
-        # if we abort.., shall be 0 again
-        e.tpabort(0)
-
-        with db.cursor() as cursor:
-            cursor.execute("SELECT count(*) FROM pyaccounts")
-            self.assertEqual(cursor.fetchone()[0], 0)
-
-        e.tpbegin(99, 0)
-        # do some insert
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO1", "T_LONG_FLD":100}})
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO2", "T_LONG_FLD":102}})
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO3", "T_LONG_FLD":103}})
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO4", "T_LONG_FLD":104}})
-
-        e.tpcommit(0)
-
-        # commit was ok, have the records:
-        with db.cursor() as cursor:
-            cursor.execute("SELECT count(*) FROM pyaccounts")
-            self.assertEqual(cursor.fetchone()[0], 4)
-
-        e.tpbegin(99, 0)
-
-        # do some insert, fails with TPESVCERR as exception is not catched
-        try:
-            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO1", "T_LONG_FLD":100}})
-        except e.XatmiException as ex:
-            self.assertEqual(ex.code,e.TPESVCERR)
-        else:
-            self.assertEqual(True,False)
-        
-        # abort-only:
-        try:
+            e.tplog_info("clean up the DB...")
+            db = cx_Oracle.connect(handle=e.xaoSvcCtx())
+            e.tpbegin(99, 0)
+            with db.cursor() as cursor:
+                 cursor.execute("delete from pyaccounts")
             e.tpcommit(0)
-        except e.XatmiException as ex:
-            self.assertEqual(ex.code,e.TPEABORT)
-        else:
-            self.assertEqual(True,False)
+           
+            # check 0...
+            e.tpbegin(99, 0)
+            with db.cursor() as cursor:
+                cursor.execute("SELECT count(*) FROM pyaccounts")
+                self.assertEqual(cursor.fetchone()[0], 0)
+            e.tpcommit(0)
+      
 
-        # change the commit control:
-        e.tpscmt(e.TP_CMT_LOGGED)
-        e.tpbegin(99, 0)
-        e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO5", "T_LONG_FLD":105}})
-        e.tpcommit(0)
+            e.tpbegin(99, 0)
+            # do some insert
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO1", "T_LONG_FLD":100}})
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO2", "T_LONG_FLD":102}})
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO3", "T_LONG_FLD":103}})
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO4", "T_LONG_FLD":104}})
+
+            # check account, shall be 4...
+            with db.cursor() as cursor:
+                cursor.execute("SELECT count(*) FROM pyaccounts")
+                self.assertEqual(cursor.fetchone()[0], 4)
+
+            # if we abort.., shall be 0 again
+            e.tpabort(0)
+
+            with db.cursor() as cursor:
+                cursor.execute("SELECT count(*) FROM pyaccounts")
+                self.assertEqual(cursor.fetchone()[0], 0)
+
+            e.tpbegin(99, 0)
+            # do some insert
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO1", "T_LONG_FLD":100}})
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO2", "T_LONG_FLD":102}})
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO3", "T_LONG_FLD":103}})
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO4", "T_LONG_FLD":104}})
+
+            e.tpcommit(0)
+
+            # commit was ok, have the records:
+            with db.cursor() as cursor:
+                cursor.execute("SELECT count(*) FROM pyaccounts")
+                self.assertEqual(cursor.fetchone()[0], 4)
+
+            e.tpbegin(99, 0)
+
+            # do some insert, fails with TPESVCERR as exception is not catched
+            try:
+                e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO1", "T_LONG_FLD":100}})
+            except e.XatmiException as ex:
+                self.assertEqual(ex.code,e.TPESVCERR)
+            else:
+                self.assertEqual(True,False)
+            
+            # abort-only:
+            try:
+                e.tpcommit(0)
+            except e.XatmiException as ex:
+                self.assertEqual(ex.code,e.TPEABORT)
+            else:
+                self.assertEqual(True,False)
+
+            # change the commit control:
+            e.tpscmt(e.TP_CMT_LOGGED)
+            e.tpbegin(99, 0)
+            e.tpcall("DOINSERT", {"data":{"T_STRING_FLD":"HELLO5", "T_LONG_FLD":105}})
+            e.tpcommit(0)
 
         e.tpterm()
 
